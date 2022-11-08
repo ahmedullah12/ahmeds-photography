7dc51810f158150d61246468b3b194f372f0629b
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-08 21:29:16 +0600

    user register with email and password added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { FcGoogle } from 'react-icons/fc';
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Register = () => {
-    return (
-        <div className="hero  py-4  lg:py-20 bg-base-200">
+    const {user, signUpWithEmailAndPassword} =  useContext(AuthContext)
+    const handleCreateUser = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+
+        signUpWithEmailAndPassword(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err))
+    }
+  return (
+    <div className="hero  py-4  lg:py-20 bg-base-200">
       <div className="hero-content block lg:flex">
         <div>
           <img
@@ -17,7 +33,7 @@ const Register = () => {
 
         <div className="card flex-shrink-0 w-full max-w-sm  shadow-2xl p-2 lg:p-10 bg-base-100">
           <h1 className="mt-4 text-4xl text-center font-bold">Register now!</h1>
-          <div className="card-body">
+          <form onSubmit={handleCreateUser} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
@@ -54,17 +70,22 @@ const Register = () => {
                 required
               />
             </div>
-            <p  className="text-sm  mt-2 ">
-                Already have an account? Please <Link className="link text-blue-700" to='/login'>Login.</Link>
+            <p className="text-sm  mt-2 ">
+              Already have an account? Please{" "}
+              <Link className="link text-blue-700" to="/login">
+                Login.
+              </Link>
             </p>
             <div className="form-control mt-6">
-              <button type="submit" className="btn btn-primary">Register</button>
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Register;
