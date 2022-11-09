c847527dcf34172696c0fb1803aa9e85d6d19096
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-09 17:14:41 +0600

    all services showed in services route

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -6,23 +6,20 @@ const Services = () => {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/limited-services')
+        fetch('http://localhost:5000/services')
         .then(res => res.json())
         .then(data => setServices(data));
-    },[setServices])
+    },[])
     return (
         <div className='mt-10 text-center '>
-            <h2 className='text-4xl text-orange-400 font-bold'>My Services</h2>
-            <p className='text-lg my-4'>These are  the latest photography services I provide. You can see all the service by clicking see all button.</p>
-            
+            <h2 className='text-4xl text-orange-400 font-bold'>My All Services</h2>
+            <p className='text-lg my-4'>These are  the all photography services I provide. You can select what you like from this services.</p>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-start'>
                 {
                     services.map(service => <Service key={service._id} service={service}></Service>)
                 }
             </div>
-            <div>
-            <Link to='/services'><button className=' mt-5 px-10 items-center rounded text-white btn btn-warning'>See All</button></Link>
-            </div>
+            
         </div>
     );
 };
