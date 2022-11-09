b7578ed9bb4b91d5c90e84003fd59cb43e1f9669
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-09 16:15:08 +0600

    3 services added in home page

---
+++ src/Pages/Home/Services/Service.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {BsArrowRight} from "react-icons/bs"
+import { Link } from "react-router-dom";
+
+const Service = ({ service }) => {
+    const {_id, title, image, price, description} = service;
+  return (
+    <div className="card w-full bg-base-100 shadow-xl">
+      <figure>
+        <img className="w-80 h-52 mt-10 rounded"  src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title} 
+        </h2>
+        <p>{description.slice(0, 100)} ...</p>
+        <p className="text-orange-600">Price: {price}</p>
+        <Link to={`/services/${_id}`}>
+            <button className="w-3/4 mt-3 ml-20 btn btn-secondary">More Details <BsArrowRight className="inline ml-4"></BsArrowRight></button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/limited-services')
+        .then(res => res.json())
+        .then(data => setServices(data));
+    },[setServices])
     return (
         <div>
+            <h2>My Services</h2>
+            <p>These are are the photography services I provide. You can select from these servies</p>
             
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service => <Service key={service._id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
