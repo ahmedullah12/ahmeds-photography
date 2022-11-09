7fee7aa186e7e8978b100c3bc5edf9d335dca867
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-09 16:33:14 +0600

    banner added in home page

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,8 +2,51 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-        
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src="https://i.ibb.co/4F5Qdm7/car1.jpg"  className="w-full" alt="" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src="https://i.ibb.co/F40DY2v/gv-Q9-Nh-QP8wbb-M32j-Xy4-V3j-min.jpg"  className="w-full" alt="" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src="https://i.ibb.co/cD3Bw8R/car3.jpg"  className="w-full" alt="" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src="https://placeimg.com/800/200/arch"  className="w-full" alt="" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
